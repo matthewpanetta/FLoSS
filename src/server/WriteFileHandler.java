cd566b59c15e4a46c0e19764ea221a84cbefd13b
Matt Panetta <mdp5280@psu.edu>
2014-09-27 17:48:36 -0400

    Extracted code into various methods for readability

--- src/server/WriteFileHandler.java
+++ src/server/WriteFileHandler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.JFileChooser;
@@ -15,6 +16,46 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class WriteFileHandler {
 	String result = "";
+	
+	public void writeFile() throws IOException {
+		File[] files = getFiles();
+		
+		String fileName = "";
+		
+		HttpURLConnection httpUrlConnection;
+
+		FileInputStream fis;
+		BufferedInputStream bis;
+		
+		long byteLength;
+		
+		for (File f : files) {
+			fileName = f.getName();		// Get the file's name
+			fileName.trim();			// Eliminate whitespace before and after string
+			fileName = fileName.replaceAll("\\s", "");	// Eliminate spaces in file name
+			
+			httpUrlConnection = setupConnection(fileName);
+			
+			fis = new FileInputStream(f);
+			bis = new BufferedInputStream(fis);
+			
+			byteLength = f.length();	// Get the length of the file.
+			
+			writeToOutput(httpUrlConnection, bis, byteLength);
+			
+			readStream(httpUrlConnection);
+
+			fis.close();
+		}
+	}
+	
+	public HttpURLConnection setupConnection(String fileName) throws MalformedURLException, IOException {
+		HttpURLConnection httpUrlConnection = (HttpURLConnection) new URL("http://65.185.85.1//writeFile.php?filename=" + fileName).openConnection();	// Specify the URL of the writeFile script on the server.
+		httpUrlConnection.setDoOutput(true);
+		httpUrlConnection.setRequestMethod("POST");	// PHP script uses POST.
+		
+		return httpUrlConnection;
+	}
 	
 	public File[] getFiles() {
 		// Opens a file chooser dialog GUI where the user selects which file(s) they would like to upload.
@@ -37,51 +78,30 @@ public class WriteFileHandler {
 		return null;
 	}
 	
-	public void writeFile() throws IOException {
-		File[] files = getFiles();
+	public void writeToOutput(HttpURLConnection httpUrlConnection, BufferedInputStream bis, long byteLength) throws IOException {
+		OutputStream os = httpUrlConnection.getOutputStream();
 		
-		String fileName = "";
-		String s = "";
-		
-		HttpURLConnection httpUrlConnection;
-		
-		OutputStream os;
-		FileInputStream fis;
-		BufferedInputStream bis;
-		
-		long byteLength;
-		
-		for (File f : files) {
-			fileName = f.getName();		// Get the file's name
-			fileName.trim();			// Eliminate whitespace before and after string
-			fileName = fileName.replaceAll("\\s", "");	// Eliminate spaces in file name
-			
-			httpUrlConnection = (HttpURLConnection) new URL("http://65.185.85.1//writeFile.php?filename=" + fileName).openConnection();	// Specify the URL of the writeFile script on the server.
-			httpUrlConnection.setDoOutput(true);
-			httpUrlConnection.setRequestMethod("POST");	// PHP script uses POST.
-			
-			os = httpUrlConnection.getOutputStream();
-			fis = new FileInputStream(f);
-			bis = new BufferedInputStream(fis);
-			
-			byteLength = f.length();	// Get the length of the file.
-			
-			for (int i = 0; i < byteLength; i++) {
-				os.write(bis.read());	// For each byte that input stream reads, write to output stream. This ensures the entire file is uploaded to the file.
-			}
-			
-			os.close();
-			BufferedReader in = new BufferedReader(new InputStreamReader(httpUrlConnection.getInputStream()));
-			
-			s = null;
-			while ((s = in.readLine()) != null) {
-				result = s;
-			}
-			
-			in.close();
-			fis.close();
+		for (int i = 0; i < byteLength; i++) {
+			os.write(bis.read());	// For each byte that input stream reads, write to output stream. This ensures the entire file is uploaded to the file.
 		}
+		
+		os.close();
 	}
+	
+	public void readStream(HttpURLConnection httpUrlConnection) throws IOException {
+		// Read the echo statement in the PHP file.
+		String s;
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(httpUrlConnection.getInputStream()));
+		
+		s = null;
+		while ((s = in.readLine()) != null) {
+			result = s;
+		}
+		
+		in.close();
+	}
+	
 	
 	public String getSuccess() {
 		return result;
