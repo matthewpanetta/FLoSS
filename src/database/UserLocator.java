d815dc6048c74e5e9c4fae8b76d200e97cb600bd
Matt Panetta <mdp5280@psu.edu>
2014-11-11 22:20:16 -0500

    Added user search functionality

---
+++ src/database/UserLocator.java
@@ -0,0 +1,56 @@
+package database;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import client.User;
+
+public class UserLocator {
+	private DBConnection connection = DBConnection.getInstance();
+	private String userName;
+	private User user;
+	
+	public UserLocator() {
+		
+	}
+	
+	public void setUser(String userName) {
+		this.userName = userName;
+	}
+	
+	public User search() {
+		try {
+			connection.connect();
+			PreparedStatement statement = connection.getConnection().prepareStatement("SELECT * FROM user WHERE userName LIKE(?);");
+			statement.setString(1, userName);
+			
+			ResultSet result = statement.executeQuery();
+			
+			
+			if(!result.isBeforeFirst()) {
+				user = null;
+			}
+			
+			else {
+				result.next();
+				user = new User(result.getString(1), result.getString(2));		// this seems like bad practice...oh well!
+			}
+			
+			connection.close();
+			return user;
+			
+		}
+		catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	// ---------- TEST CASE ---------- //
+	public static void main(String[] args) {
+		UserLocator ul = new UserLocator();
+		ul.setUser("mp755");
+		User user = ul.search();
+		
+		System.out.println(user.getUserName() + "\n" + user.getUserPassword());
+	}
+}
