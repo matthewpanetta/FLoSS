727cbdd7419d058d8655a61dbca465472d0ba026
Matt Panetta <mdp5280@psu.edu>
2014-11-13 11:44:28 -0500

    Added file ID field

--- src/client/File.java
+++ src/client/File.java
@@ -3,14 +3,16 @@ package client;
 import java.util.Date;
 
 public class File {
-	private String fileName;
-	private String filePath;
-	private String owner;
-	private Date modifiedDate;
-	private Date creationDate;
+	private int		fileID;
+	private String	fileName;
+	private String	filePath;
+	private String	owner;
+	private Date	modifiedDate;
+	private Date	creationDate;
 	
-	public File(String fileName, String filePath, String userName, Date modifiedDate, Date creationDate) {
+	public File(int fileID, String fileName, String filePath, String userName, Date modifiedDate, Date creationDate) {
 		super();
+		this.fileID = fileID;
 		this.fileName = fileName;
 		this.filePath = filePath;
 		this.owner = userName;
@@ -27,32 +29,49 @@ public class File {
 	public String getFileName() {
 		return fileName;
 	}
+	
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
+	
 	public String getFilePath() {
 		return filePath;
 	}
+	
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
+	
 	public String getOwner() {
 		return owner;
 	}
+	
 	public void setOwner(String userName) {
 		this.owner = userName;
 	}
+	
 	public Date getModifiedDate() {
 		return modifiedDate;
 	}
+	
 	public void setModifiedDate(Date modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
+	
 	public Date getCreationDate() {
 		return creationDate;
 	}
+	
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
+	}
+	
+	public int getFileID() {
+		return fileID;
+	}
+	
+	public void setFileID(int fileID) {
+		this.fileID = fileID;
 	}
 	
 }
