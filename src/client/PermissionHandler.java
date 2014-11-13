4a837848161c12a287c7c6481541f66366e061bb
Matt Panetta <mdp5280@psu.edu>
2014-11-13 15:03:13 -0500

    'Fixed' errors

--- src/client/PermissionHandler.java
+++ src/client/PermissionHandler.java
@@ -5,25 +5,25 @@ public class PermissionHandler extends Handler{
 	
 	public void addPermission(User owner, User friend, String fileName, String type){
 		// Give a user read permission to a file.
-		permission = new Permission('r');
+		//permission = new Permission('r');
 		super.getDatabaseAdapter().addPermission(owner, permission);
 		
 	}
 	public void removePermission(User owner, User friend, String fileName, String type){
 		// Remove the read permission from the file for a specific user.
-		permission = new Permission('d');
+		//permission = new Permission('d');
 		super.getDatabaseAdapter().removePermission(owner, permission);
 		
 	}
 	public void addOwner(User owner){
 		// Add an owner to a file.
-		permission = new Permission('o');
+		//permission = new Permission('o');
 		super.getDatabaseAdapter().addOwner(owner, permission);
 		
 	}
 	public void addAccessorToGroup(User friend, Group g){
 		// Add a user to the group
-		permission = new Permission('a');
+		//permission = new Permission('a');
 		super.getDatabaseAdapter().addAccessor(friend, g, permission);
 		
 	}
