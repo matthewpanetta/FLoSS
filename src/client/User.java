4f81a98d2c69c13e62356806fba71b1d63103928
Matt Panetta <mdp5280@psu.edu>
2014-11-15 10:08:51 -0500

    Updated user class to include all fields

--- src/client/User.java
+++ src/client/User.java
@@ -1,24 +1,96 @@
 package client;
 
+import java.util.Date;
+
 public class User {
 	private String userName;
-	private String userPassword;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Date bithDate;
+	private char gender;
+	private char middleInitial;
 	
-	public User(String userName, String userPassword) {
+	public User(String userName, String password) {
 		super();
 		this.userName = userName;
-		this.userPassword = userPassword;
+		this.password = password;
 	}
+	
+	public User(String userName, String password, String firstName, String lastName, String email, Date bithDate, char gender, char middleInitial) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.bithDate = bithDate;
+		this.gender = gender;
+		this.middleInitial = middleInitial;
+	}
+
 	public String getUserName() {
 		return userName;
 	}
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public String getUserPassword() {
-		return userPassword;
+	
+	public String getPassword() {
+		return password;
 	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBithDate() {
+		return bithDate;
+	}
+
+	public void setBithDate(Date bithDate) {
+		this.bithDate = bithDate;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public char getMiddleInitial() {
+		return middleInitial;
+	}
+
+	public void setMiddleInitial(char middleInitial) {
+		this.middleInitial = middleInitial;
 	}
 }
