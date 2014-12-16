34726eef157eb140f0e0ffec6892b850bb88e9ce
Martin Seibert <mas6553@psu.edu>
2014-12-16 16:10:42 -0500

    added profile capability

--- src/gui/LoginGUI.java
+++ src/gui/LoginGUI.java
@@ -211,8 +211,8 @@ public class LoginGUI extends javax.swing.JFrame {
                 download.refreshFileList();
                 download.refreshFriends();
                 download.refreshFriendsList();
-                download.getRecentFiles();
-                download.getRecentCollabs();
+//                download.getRecentFiles();
+  //              download.getRecentCollabs();
                 download.setVisible(true);
                 dispose();
             }
